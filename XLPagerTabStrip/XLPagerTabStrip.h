81efb750b41d238260cc96fd6c41ec767c3063f2
yu840915 <mike@17.media>
2016-08-29 13:24:13 +0800

    Edit umbrella header

--- XLPagerTabStrip/XLPagerTabStrip.h
+++ XLPagerTabStrip/XLPagerTabStrip.h
@@ -16,4 +16,11 @@ FOUNDATION_EXPORT const unsigned char XLPagerTabStripVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <XLPagerTabStrip/PublicHeader.h>
 
-
+#import <XLPagerTabStrip/XLTwitterPagerTabStripViewController.h>
+#import <XLPagerTabStrip/XLPagerTabStripViewController.h>
+#import <XLPagerTabStrip/XLBarView.h>
+#import <XLPagerTabStrip/XLSegmentedPagerTabStripViewController.h>
+#import <XLPagerTabStrip/XLBarPagerTabStripViewController.h>
+#import <XLPagerTabStrip/XLButtonBarPagerTabStripViewController.h>
+#import <XLPagerTabStrip/XLButtonBarView.h>
+#import <XLPagerTabStrip/XLButtonBarViewCell.h>
